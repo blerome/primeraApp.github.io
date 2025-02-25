905539d58faa96e680f572ff9021ba39accb5e82
blerome <105025286+blerome@users.noreply.github.com>
2025-02-25 09:55:30 -0500

    Update iniciado.component.ts

--- src/app/iniciado/iniciado.component.ts
+++ src/app/iniciado/iniciado.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { BarraBusquedaComponent } from "../barra-busqueda/barra-busqueda.component";
+import { FiltradoComponent } from '../filtrado/filtrado.component';
+import { AltaMateriaComponent } from '../alta-materia/alta-materia.component';
 
 @Component({
   selector: 'app-iniciado',
   templateUrl: './iniciado.component.html',
-  styleUrl: './iniciado.component.css'
+  styleUrl: './iniciado.component.css',
+  imports: [BarraBusquedaComponent, FiltradoComponent,AltaMateriaComponent]
 })
 export class IniciadoComponent { }
